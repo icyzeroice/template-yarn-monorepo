7cae55a73cc73047c9af0a94aac0bf689781b687
icyzeroice <ziv3@outlook.com>
2022-03-05 22:57:48 +0800

    lite

--- packages/app-vallina/src/main.ts
+++ packages/app-vallina/src/main.ts
@@ -4,7 +4,7 @@ import './style.css'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
-function run() {
+function run () {
   app.innerHTML = `
     <h1>Hello Vite!</h1>
     <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation ${timeline()}</a>
