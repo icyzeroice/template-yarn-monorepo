6c29b7576298445dbfb4fab74b0dfe6b6563426b
fkworld <fkworld@foxmail.com>
2022-08-30 10:59:55 +0800

    chore: add eslint,prettier,husky,commitlint,lint-staged

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint', 'simple-import-sort', 'import'],
+  rules: {
+    // import/export 排序，参考 https://github.com/lydell/eslint-plugin-simple-import-sort#example-configuration
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+  },
+}
---
+++ .lintstagedrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  '**/*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
+  '**/*.{md,json,html,css,less,scss,yaml,yml}': ['prettier --write'],
+}
